d11290f76c8505f0840b7ca7d6a589e4315b003e
harjulianto <feriharjulianto@gmail.com>
2014-09-07 19:10:10 +0700

    newest

---
+++ lib/top-nav.php
@@ -0,0 +1,20 @@
+<nav class="top-bar animate-dropdown">
+    <div class="container">
+        <div class="col-xs-12 col-sm-6 no-margin">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="about.html">Tentang Kami</a></li>
+                <li><a href="faq.html">FAQ</a></li>
+                <li><a href="contact.html">Kontak</a></li>
+            </ul>
+        </div><!-- /.col -->
+
+        <div class="col-xs-12 col-sm-6 no-margin">
+            <ul class="right">
+                <li><a href="authentication.html">Daftar</a></li>
+                <li><a href="authentication.html">Login</a></li>
+            </ul>
+        </div><!-- /.col -->
+    </div><!-- /.container -->
+</nav><!-- /.top-bar -->
